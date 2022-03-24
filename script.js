a5f1c34679eb8fb1a11bc6b2f75100d716f4ada5
Sitpi Rajendran <39497470+SitpiRajendran@users.noreply.github.com>
2022-03-23 21:20:56 -0400

    [UPD] - Mobile Version v1

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ function openMenu(e) {
     Style = window.getComputedStyle(elem)
     right = Style.getPropertyValue("right");
 
+    console.log(right)
     if (right == "-50px") {
         elem.style.right = "-450px";
-    } else {
+    } else if (right == "-450px") {
         elem.style.right = "-50px";
+    } else if (right.includes("-")) {
+        elem.style.right = "0px";
+    } else if (right == "0px") {
+        elem.style.right = "-100vw"
     }
 }
